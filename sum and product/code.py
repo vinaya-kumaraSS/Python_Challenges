f7cc3b5a7a6ddb581e438b2e481a60f0b6f37e00
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2023-09-12 21:05:58 +0530

    Added new file

---
+++ sum and product/code.py
@@ -0,0 +1,8 @@
+num1=int(input("enter the first number :"))
+num2=int(input("enter the second number :"))
+s=num1+num2
+p=num1*num2
+if p <=1000:
+    print(f"the product of the given number is {p}")
+else:
+    print(f"the sum of the given number id {s}")
